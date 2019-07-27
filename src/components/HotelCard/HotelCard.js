25614f5790fb1a44c424fce0d9d0446c7858b794
Sameer Ismail <sam@samism.io>
2019-07-27 17:12:27 -0500

    Replace warning for dangerouslySetInnerHTML with utility function.

--- src/components/HotelCard/HotelCard.js
+++ src/components/HotelCard/HotelCard.js
@@ -1,35 +1,40 @@
 import React from 'react';
 import './HotelCard.style.scss';
 
-const HotelCard = ({ hotel }) => {
-  return (
-    <section className="hotel-card">
-      <figure
-        className="image"
-        style={{
-          backgroundImage: `url(${hotel.hotelStaticContent.mainImage.url})`
-        }}
-      />
-      <section className="hotel-details">
-        <h5 className="hotel-name">{hotel.hotelStaticContent.name}</h5>
-        <p className="location">
-          {hotel.hotelStaticContent.neighborhoodName}
-        </p>
-      </section>
-      <section className="price-details">
-        <span className="price">
-          <span
-            dangerouslySetInnerHTML={{
-              __html: hotel.lowestAveragePrice.symbol
-            }}
-          />
-          {hotel.lowestAveragePrice.amount}
-        </span>
-        <span className="rewards">{hotel.rewards.miles} miles</span>
-        <button className="button">Select</button>
-      </section>
-    </section>
-  );
+/**
+ * Credit: https://ourcodeworld.com
+ *
+ */
+const decodeHtmlEntityString = entity => {
+  return entity.replace(/&#(\d+);/g, (match, dec) => {
+    return String.fromCharCode(dec);
+  });
 };
+
+const HotelCard = ({ hotel }) => (
+  <section className="hotel-card">
+    <figure
+      className="image"
+      style={{
+        backgroundImage: `url(${hotel.hotelStaticContent.mainImage.url})`
+      }}
+    />
+    <section className="hotel-details">
+      <h5 className="hotel-name">{hotel.hotelStaticContent.name}</h5>
+      <p className="location">{hotel.hotelStaticContent.neighborhoodName}</p>
+    </section>
+    <section className="price-details">
+      <span className="price">
+        {`${decodeHtmlEntityString(hotel.lowestAveragePrice.symbol)}${
+          hotel.lowestAveragePrice.amount
+        }`}
+      </span>
+      <span className="rewards">{hotel.rewards.miles} miles</span>
+      <button className="button" type="button">
+        Select
+      </button>
+    </section>
+  </section>
+);
 
 export default HotelCard;
