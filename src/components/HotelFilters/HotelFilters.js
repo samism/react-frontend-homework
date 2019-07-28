b769c60ff68949df036f2d0ca29bc972142f907d
Sameer Ismail <sam@samism.io>
2019-07-28 16:41:59 -0500

    Wrap component to retain its original height.

--- src/components/HotelFilters/HotelFilters.js
+++ src/components/HotelFilters/HotelFilters.js
@@ -3,23 +3,25 @@ import React from 'react';
 import './HotelFilters.style.scss';
 
 const HotelFilters = ({ filters: [filterByName, sortByOption] }) => (
-  <div className="filters">
-    Hotel name
-    <input type="text" className="input" onChange={filterByName.bind(this)} />
-    Price
-    <select
-      name="sort-hotels-select"
-      className="select"
-      onChange={sortByOption.bind(this)}
-    >
-      <option value="recommended">Recommended</option>
-      <option value="price-asc">Price low-to-high</option>
-      <option value="price-desc">Price high-to-low</option>
-    </select>
-    <button className="button" type="button">
-      Reset
-    </button>
-  </div>
+  <aside>
+    <div className="filters">
+      Hotel name
+      <input type="text" className="input" onChange={filterByName.bind(this)} />
+      Price
+      <select
+        name="sort-hotels-select"
+        className="select"
+        onChange={sortByOption.bind(this)}
+      >
+        <option value="recommended">Recommended</option>
+        <option value="price-asc">Price low-to-high</option>
+        <option value="price-desc">Price high-to-low</option>
+      </select>
+      <button className="button" type="button">
+        Reset
+      </button>
+    </div>
+  </aside>
 );
 
 export default HotelFilters;
