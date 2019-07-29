c9825503ce9c2bcb42fa9b71de86478f6cf03a44
Sameer Ismail <sam@samism.io>
2019-07-28 21:56:00 -0500

    Add tests for filtering & sorting.

--- src/components/HotelFilters/HotelFilters.spec.js
+++ src/components/HotelFilters/HotelFilters.spec.js
@@ -1,17 +1,86 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
+
+import sampleData from './sampleData';
 import HotelFilters from './HotelFilters';
 
 describe('HotelFilters', () => {
-  const wrapper = shallow(
+  const resultingList = [];
+
+  const wrapper = mount(
     <HotelFilters
-      hotels={[]}
-      listHandler={() => {}}
+      originalData={sampleData}
+      hotels={sampleData}
+      listHandler={results => resultingList.push(...results)}
       resetHotelList={() => {}}
     />
   );
 
+  afterEach(() => {
+    resultingList.splice(0, resultingList.length);
+  });
+
   it('renders the component', () => {
     expect(wrapper.find('.filters').exists()).toBe(true);
+  });
+
+  it('returns filtered data given some capitalized input', () => {
+    wrapper.find('.input').simulate('change', { target: { value: 'H' } });
+
+    expect(
+      resultingList
+        .map(({ hotelStaticContent: { name } }) => name)
+        .every(hotelName =>
+          [
+            'Hampton Inn Chicago-Naperville',
+            'Homewood Suites by Hilton Chicago-Lincolnshire'
+          ].includes(hotelName)
+        )
+    );
+  });
+
+  it('returns filtered data given some lowercased input', () => {
+    wrapper.find('.input').simulate('change', { target: { value: 'h' } });
+
+    expect(
+      resultingList
+        .map(({ hotelStaticContent: { name } }) => name)
+        .every(hotelName =>
+          [
+            'Hampton Inn Chicago-Naperville',
+            'Homewood Suites by Hilton Chicago-Lincolnshire'
+          ].includes(hotelName)
+        )
+    );
+  });
+
+  it('returns sorted data by price (ascending)', () => {
+    wrapper
+      .find('.select')
+      .simulate('change', { target: { value: 'price-asc' } });
+
+    expect(
+      resultingList.map(({ lowestAveragePrice: { amount } }) => amount)
+    ).toEqual([123, 987, 947, 234].sort((a, b) => a - b));
+  });
+
+  it('returns sorted data by price (descending)', () => {
+    wrapper
+      .find('.select')
+      .simulate('change', { target: { value: 'price-desc' } });
+
+    expect(
+      resultingList.map(({ lowestAveragePrice: { amount } }) => amount)
+    ).toEqual([123, 987, 947, 234].sort((a, b) => b - a));
+  });
+
+  it('returns sorted data by recommended (rating, desc)', () => {
+    wrapper
+      .find('.select')
+      .simulate('change', { target: { value: 'recommended' } });
+
+    expect(
+      resultingList.map(({ hotelStaticContent: { rating } }) => rating)
+    ).toEqual([8.5, 9.1, 9.2, 8.8].sort((a, b) => b - a));
   });
 });
---
+++ src/components/HotelFilters/sampleData.js
@@ -0,0 +1,174 @@
+export default [
+  {
+    id: '35277',
+    rewards: {
+      miles: 9000
+    },
+    lowestAveragePrice: {
+      currency: 'USD',
+      symbol: '&#36;',
+      amount: 947
+    },
+    hotelStaticContent: {
+      hotelId: 35277,
+      languageCode: 'en',
+      mainImage: {
+        category: 'EXTERIOR',
+        url:
+          'http://d2whcypojkzby.cloudfront.net/imageRepo/3/0/36/937/326/CHINVHX_Hotel_front_view_S.jpg',
+        source: 'SOURCE1'
+      },
+      name: 'Hampton Inn Chicago-Naperville',
+      neighborhoodName: 'Naperville',
+      address: {
+        line1: '1087 East Diehl Road',
+        line2: null,
+        city: 'Naperville',
+        stateName: 'Illinois',
+        stateCode: 'IL',
+        countryName: 'United States',
+        countryCode: 'US',
+        zipCode: '60563',
+        latitude: 41.80294,
+        longitude: -88.13032,
+        timeZoneId: 'America/Chicago'
+      },
+      stars: 3,
+      rating: 8.8,
+      numberOfReviews: 141,
+      brandCode: '1851',
+      brandName: 'Hampton by Hilton',
+      propertyType: 204,
+      propertyTypeName: 'Hotel'
+    }
+  },
+  {
+    id: '908',
+    rewards: {
+      miles: 8000
+    },
+    lowestAveragePrice: {
+      currency: 'USD',
+      symbol: '&#36;',
+      amount: 234
+    },
+    hotelStaticContent: {
+      hotelId: 908,
+      languageCode: 'en',
+      mainImage: {
+        category: 'EXTERIOR',
+        url:
+          'http://d2whcypojkzby.cloudfront.net/imageRepo/6/0/96/743/239/chidt-exterior-0227-hor-clsc_S.jpg',
+        source: 'SOURCE1'
+      },
+      name: 'Chicago Marriott Downtown Magnificent Mile',
+      neighborhoodName: 'Magnificent Mile',
+      address: {
+        line1: '540 North Michigan Avenue',
+        line2: null,
+        city: 'Chicago',
+        stateName: 'Illinois',
+        stateCode: 'IL',
+        countryName: 'United States',
+        countryCode: 'US',
+        zipCode: '60611',
+        latitude: 41.89216,
+        longitude: -87.62443,
+        timeZoneId: 'America/Chicago'
+      },
+      stars: 4,
+      rating: 8.5,
+      numberOfReviews: 885,
+      brandCode: '1080',
+      brandName: 'Marriott Hotels & Resorts',
+      propertyType: 204,
+      propertyTypeName: 'Hotel'
+    }
+  },
+  {
+    id: '87390',
+    rewards: {
+      miles: 12000
+    },
+    lowestAveragePrice: {
+      currency: 'USD',
+      symbol: '&#36;',
+      amount: 123
+    },
+    hotelStaticContent: {
+      hotelId: 87390,
+      languageCode: 'en',
+      mainImage: {
+        category: 'EXTERIOR',
+        url:
+          'http://d2whcypojkzby.cloudfront.net/imageRepo/1/0/16/99/327/012857_A_P.jpg',
+        source: 'SOURCE1'
+      },
+      name: 'Homewood Suites by Hilton Chicago-Lincolnshire',
+      neighborhoodName: 'Lincolnshire',
+      address: {
+        line1: '10 Westminster Way',
+        line2: null,
+        city: 'Lincolnshire',
+        stateName: 'Illinois',
+        stateCode: 'IL',
+        countryName: 'United States',
+        countryCode: 'US',
+        zipCode: '60069',
+        latitude: 42.19918,
+        longitude: -87.89188,
+        timeZoneId: 'America/Chicago'
+      },
+      stars: 3,
+      rating: 9.1,
+      numberOfReviews: 110,
+      brandCode: '1982',
+      brandName: 'Homewood Suites by Hilton',
+      propertyType: 204,
+      propertyTypeName: 'Hotel'
+    }
+  },
+  {
+    id: '40623',
+    rewards: {
+      miles: 5000
+    },
+    lowestAveragePrice: {
+      currency: 'USD',
+      symbol: '&#36;',
+      amount: 987
+    },
+    hotelStaticContent: {
+      hotelId: 40623,
+      languageCode: 'en',
+      mainImage: {
+        category: null,
+        url:
+          'http://www.tnetnoc.com//hotelphotos/892/36892/2241284-The-Talbott-Hotel-Hotel-Exterior-1-DEF.jpg',
+        source: 'SOURCE2'
+      },
+      name: 'The Talbott Hotel',
+      neighborhoodName: 'Magnificent Mile',
+      address: {
+        line1: '20 E Delaware Place',
+        line2: null,
+        city: 'Chicago',
+        stateName: 'Illinois',
+        stateCode: 'IL',
+        countryName: 'United States',
+        countryCode: 'US',
+        zipCode: '60611',
+        latitude: 41.89919,
+        longitude: -87.62749,
+        timeZoneId: 'America/Chicago'
+      },
+      stars: 4,
+      rating: 9.2,
+      numberOfReviews: 544,
+      brandCode: '8127',
+      brandName: 'Joie De Vivre hotels',
+      propertyType: 204,
+      propertyTypeName: 'Hotel'
+    }
+  }
+];
