cee5e7063f7ea168dd88e0b25c4e6b9bbbd337b4
Sameer Ismail <sam@samism.io>
2019-07-28 20:57:37 -0500

    Adjust props from previous refactor.

--- src/components/HotelFilters/HotelFilters.spec.js
+++ src/components/HotelFilters/HotelFilters.spec.js
@@ -3,7 +3,13 @@ import { shallow } from 'enzyme';
 import HotelFilters from './HotelFilters';
 
 describe('HotelFilters', () => {
-  const wrapper = shallow(<HotelFilters filters={[() => {}, () => {}]} />);
+  const wrapper = shallow(
+    <HotelFilters
+      hotels={[]}
+      listHandler={() => {}}
+      resetHotelList={() => {}}
+    />
+  );
 
   it('renders the component', () => {
     expect(wrapper.find('.filters').exists()).toBe(true);
