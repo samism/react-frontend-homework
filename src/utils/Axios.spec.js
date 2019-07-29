ed17254f1f661c5257ef3e62bcfc4a3ae8a57fe2
Sameer Ismail <sam@samism.io>
2019-07-28 22:33:06 -0500

    Add tests for singleton, retrying on 500.

---
+++ src/utils/Axios.spec.js
@@ -0,0 +1,27 @@
+import nock from 'nock';
+
+import Axios from './Axios';
+
+describe('Axios', () => {
+  it('is a singleton', () => {
+    expect(Axios).toBeTruthy();
+  });
+
+  it('is able to retry the request given an occasional 500 error', async () => {
+    const url = 'http://example.com/';
+
+    const requests = [
+      nock(url)
+        .get('/')
+        .reply(500),
+      nock(url)
+        .get('/')
+        .reply(200, 'helloworld')
+    ];
+
+    const res = await Axios.get(url);
+    expect(res.data).toEqual('helloworld');
+
+    requests.forEach(s => s.done());
+  });
+});
