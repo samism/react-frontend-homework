307835050da17eaec2c3cd8f42315c360078085e
Sameer Ismail <sam@samism.io>
2019-07-28 21:49:24 -0500

    Disable all form fields if hotels failed to load + CSS to reflect this.

--- src/components/HotelFilters/HotelFilters.js
+++ src/components/HotelFilters/HotelFilters.js
@@ -6,25 +6,32 @@ import filterByName from './filters';
 import sortByOption from './sorts';
 
 const HotelFilters = ({ hotels, listHandler, resetHotelList }) => (
-  <aside>
+  <aside className={!hotels.length && 'input-disabled'}>
     <div className="filters">
       Hotel name
       <input
         type="text"
         className="input"
         onChange={e => filterByName(e, hotels, listHandler)}
+        disabled={!hotels.length}
       />
       Price
       <select
         name="sort-hotels-select"
         className="select"
         onChange={e => sortByOption(e, hotels, listHandler)}
+        disabled={!hotels.length}
       >
         <option value="recommended">Recommended</option>
         <option value="price-asc">Price low-to-high</option>
         <option value="price-desc">Price high-to-low</option>
       </select>
-      <button className="button" type="button" onClick={resetHotelList}>
+      <button
+        className="button"
+        type="button"
+        onClick={resetHotelList}
+        disabled={!hotels.length}
+      >
         Reset
       </button>
     </div>
